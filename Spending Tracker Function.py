3939ce82bf2215f83a6ac91aaf93167ba0900b94
aniketa1612-oss <aniketagrawal161@gmail.com>
2025-10-14 15:33:16 +0530

    Update Spending Tracker Function.py

--- Spending Tracker Function.py
+++ Spending Tracker Function.py
@@ -100,12 +100,11 @@ def lambda_handler(event, context):
         return {
             "statusCode": 200,
             "headers": {
-    "Access-Control-Allow-Origin": "https://main.d1lo0rjulorvdo.amplifyapp.com",
-    "Access-Control-Allow-Methods": "GET, POST, PUT, DELETE, OPTIONS",
-    "Access-Control-Allow-Headers": "Content-Type",
-    "Content-Type": "application/json"
-}
-
+                "Access-Control-Allow-Origin": "https://main.d1lo0rjulorvdo.amplifyapp.com",
+                "Access-Control-Allow-Methods": "GET, POST, PUT, DELETE, OPTIONS",
+                "Access-Control-Allow-Headers": "Content-Type",
+                "Content-Type": "application/json"
+            },
             "body": json.dumps(response_body, default=decimal_default)
         }
 
